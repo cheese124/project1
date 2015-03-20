50015f9c5f5d731becdf084cc076d194a2565e2c
Alexander Demonaco unknown student <ad323@afsconnect1.njit.edu>
2015-03-19 21:58:11 -0400

    Inport test 2

--- index.php
+++ index.php
@@ -16,18 +16,13 @@ class Inport
     protected function __construct()
     {
  	   $lines =file('hd2013.csv');
-
-		foreach($lines as $data)
+		$file = fopen('hd2013.csv', 'r');
+		while (($line = fgetcsv($file)) !== FALSE) 
 		{
-		list($UNITID[],$INSTNM[],$ADDR[],$CITY[],$STABBR[],$ZIP[],$FIPS[],$OBEREG[],$CHFNM[],$CHFTITLE[],$GENTELE[],
-		$FAXTELE[],$EIN[],$OPEID[],$OPEFLAG[],$WEBADDR[],$ADMINURL[],$FAIDURL[],$APPLURL[],$NPRICURL[],$SECTOR[],$ICLEVEL[],
-		$CONTROL[],$HLOFFER[],$UGOFFER[],$GROFFER[],$HDEGOFR1[],$DEGGRANT[],$HBCU[],$HOSPITAL[],$MEDICAL[],$TRIBAL[],$LOCALE[],
-		$OPENPUBL[],$ACT[],$NEWID[],$DEATHYR[],$CLOSEDAT[],$CYACTIVE[],$POSTSEC[],$PSEFLAG[],$PSET4FLG[],$RPTMTH[],$IALIAS[],
-		$INSTCAT[],$CCBASIC[],$CCIPUG[],$CCIPGRAD[],$CCUGPROF[],$CCENRPRF[],$CCSIZSET[],$CARNEGIE[],$LANDGRNT[],$INSTSIZE[],
-		$CBSA[],$CBSATYPE[],$CSA[],$NECTA[],$F1SYSTYP[],$F1SYSNAM[],$F1SYSCOD[],$COUNTYCD[],$COUNTYNM[],$CNGDSTCD[],$LONGITUD[],
-		$LATITUDE[])
-		= explode(',',$data);
+  		//$line is an array of the csv elements
+  		print_r($line);
 		}
+		fclose($file);
     }
 
 }
