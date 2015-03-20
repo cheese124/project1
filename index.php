1a6d5a379e8651bd1fb9c72bba5f77644e07f0cc
Alexander Demonaco unknown student <ad323@afsconnect1.njit.edu>
2015-03-19 22:14:11 -0400

    Inport data compleated

--- index.php
+++ index.php
@@ -19,7 +19,6 @@ class Inport
 		$file = fopen('hd2013.csv', 'r');
 		while (($line = fgetcsv($file)) !== FALSE) 
 		{
-  		//$line is an array of the csv elements
   		print_r($line);
 		}
 		fclose($file);
