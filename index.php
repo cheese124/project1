90cff547937a13d4667dbc31ba4a89ca35628fe2
Alexander Demonaco unknown student <ad323@afsconnect1.njit.edu>
2015-03-21 16:57:47 -0400

    Import all the files

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-class Inport
+class Import
 {
 
     public static function getInstance()
@@ -15,16 +15,53 @@ class Inport
 
     protected function __construct()
     {
- 	   	$lines =file('hd2013.csv');
-		$file = fopen('hd2013.csv', 'r');
-		while (($line = fgetcsv($file)) !== FALSE) 
+   		//imports data
+ 	   	$data =file('hd2013.csv');
+		$datafile = fopen('hd2013.csv', 'r');
+		while (($data = fgetcsv($datafile)) !== FALSE) 
 		{
-  		print_r($line);
+  		print_r($data);
 		}
-		fclose($file);
+		fclose($datafile);
+		
+		echo '<br/>';
+		echo '<br/>';
+		
+		//imports varlist
+ 	   	$varlist =file('varlist.csv');
+		$varlistfile = fopen('varlist.csv', 'r');
+		while (($varlist = fgetcsv($varlistfile)) !== FALSE) 
+		{
+  		print_r($varlist);
+		}
+		fclose($varlistfile);
+		
+		echo '<br/>';
+		echo '<br/>';
+		
+		//imports description
+ 	   	$description =file('description.csv');
+		$descriptionfile = fopen('description.csv', 'r');
+		while (($description = fgetcsv($descriptionfile)) !== FALSE) 
+		{
+  		print_r($description);
+		}
+		fclose($descriptionfile);
+		
+		echo '<br/>';
+		echo '<br/>';
+
+		//imports frequencies
+ 	   	$frequencies =file('frequencies.csv');
+		$frequenciesfile = fopen('frequencies.csv', 'r');
+		while (($frequencies = fgetcsv($frequenciesfile)) !== FALSE) 
+		{
+  		print_r($frequencies);
+		}
+		fclose($frequenciesfile);
     }
 
 }
 
-$obj = Inport::getInstance();   
+$obj = Import::getInstance();   
 ?>
