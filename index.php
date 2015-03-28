74222d6bf1bc0142290243f505d9be52716097a8
Alexander Demonaco unknown student <ad323@afsconnect1.njit.edu>
2015-03-28 19:59:48 -0400

    Get the array ID working

--- index.php
+++ index.php
@@ -15,26 +15,35 @@ class Import
 
     protected function __construct()
     {
-   		//imports data
- 	   	$data =file('hd2013.csv');
-		$datafile = fopen('hd2013.csv', 'r');
-		while (($data = fgetcsv($datafile)) !== FALSE) 
-		{
+		$datafile = fopen("hd2013.csv","r");
+
+		$headers = fgetcsv($datafile);
+
+		while(!feof($datafile)){
+			$data = fgetcsv($datafile);
+
+			$ids[] = $data[0]; 
+			$school_array[] = array_combine($headers, $data);
+	}
+	
+	$final_array = array_combine($ids, $school_array);
+
+	fclose($file);
+
+	foreach ($final_array as $key => $val) 
+	{
 		echo "<table border='1' style='width:50%'>";
 		echo "<tr>";
-			echo "<td>";
-			echo "<a href=?UNITID=".$data[0].">".$data[1]."</a>";
-			
-			//echo "$_GET[".$data."]";
-  			//print_r($data[1]);
-  			echo "</td>";
-  		echo "</tr>";
-		}
-		fclose($datafile);
-		
-    }
+		echo "<td>";
+		echo '<a href="?page='.$key.'">'.$val["INSTNM"].'</a><br>';
+		echo "</td>";
+		echo "</tr>";
+		echo "</table>";
+	}
+	var_dump($final_array);
+	}
 
 }
-
 $obj = Import::getInstance();   
+
 ?>
