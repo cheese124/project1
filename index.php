b235212dd0f701989ba57d985452a6c92fbf0998
Alexander Demonaco unknown student <ad323@afsconnect1.njit.edu>
2015-03-29 15:43:54 -0400

    Update the values from dict.csv into the results array

--- index.php
+++ index.php
@@ -12,20 +12,20 @@ class Import
     }
     protected function __construct()
     {
-		$datafile = fopen("hd2013.csv","r");
+		$datafile = fopen("test.csv","r");
 		$index = fgetcsv($datafile);
 		while(!feof($datafile))
 		{
 			$data = fgetcsv($datafile);
 			$ids[] = $data[0]; 
-			$school_array[] = array_combine($index, $data);
+			$data_array[] = array_combine($index, $data);
 		}
-		$final_array = array_combine($ids, $school_array);
+		$hd2013_array = array_combine($ids, $data_array);
 		fclose($datafile);
 		
 		if(!$_GET['page'])
 		{
-			foreach ($final_array as $key => $value) 
+			foreach ($hd2013_array as $key => $value) 
 			{
 				echo "<table border='1' style='width:50%'>";
 				echo "<tr>";
@@ -38,19 +38,74 @@ class Import
 		}
 		else
 		{
-		$pagenum = $_GET['page'];
-		echo $pagenum;
-		$result = $final_array[$pagenum];
-		echo $result;
-		foreach ($result as $key => $value) 
-		{
-    		echo "Key: $key; Value: $value<br />\n";
-		}
-		//var_dump($final_array);
-		//$searchid = array_search ($pagenum ,$final_array);
-		//echo $searchid;
-		}
-	}
-}
+			//Prints raw data
+			$pagenum = $_GET['page'];
+			echo $pagenum;
+			$result = $hd2013_array[$pagenum];
+			echo $result.'<br />';
+			
+			foreach ($result as $key => $value) 
+			{
+    			echo "Key: $key; Value: $value<br />\n";
+			}
+			
+			
+			//inports frequencies
+			$ffile = fopen("dict.csv","r");
+			$findex = fgetcsv($ffile);
+			$i=0;
+			while(!feof($ffile))
+			{
+				$fdata = fgetcsv($ffile);
+				$fids[] = $i++; 
+				$fdata_array[] = array_combine($findex, $fdata);
+			}
+			$frequencies_array = array_combine($fids, $fdata_array);
+			fclose($ffile);
+			
+			//$finalarray =[];
+			//list rows as an array
+			
+			//list the real value for each
+			foreach ($frequencies_array as $key1 => $value1) 
+			{
+			//echo '<br />';
+			//echo $value1['varname'].' '.$value1['codevalue'].'<br />';
+			//echo "Key1: $key1; Value1: $value1<br />\n";
+				foreach ($result as $key2 => $value2)
+				{ 
+				//echo $key2;
+					if(($value1['varname'] == $key2) &($value1['codevalue'] == $value2))
+					{
+						echo '<br />';
+						$newvalue =$value1['valuelabel'];
+    					echo $newvalue;
+    					$result[$key2] = [$key2 => $newvalue];
+    				}
+				}
+				//printers values from rows
+				/*foreach ($value1 as $key2 => $value2) 
+				{
+    				echo "Key2: $key2; Value2: $value2<br />\n";
+    				
+    				//prints data results
+    				/*foreach ($result as $key3 => $value3) 
+					{
+    					if(($key3 == $value2))
+    					echo 'yay';
+					}
+					
+				}*/
+			}// frequencies for loop
+			
+			//print modified array
+			foreach ($result as $key => $value) 
+			{
+    			echo "Key: $key; Value: $value<br />\n";
+			}
+			
+		} // else
+	}//constructor
+}//class
 $obj = Import::getInstance();   
 ?>
