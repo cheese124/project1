242b663bfd2a617a9f8dfc362dd39610e8c7ee5d
Alexander Demonaco unknown student <ad323@afsconnect1.njit.edu>
2015-03-28 21:13:37 -0400

    Echo the clicked array ID

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 class Import
 {
-
     public static function getInstance()
     {
         static $instance = null;
@@ -9,41 +8,36 @@ class Import
         {
             $instance = new static();
         }
-
         return $instance;
     }
-
     protected function __construct()
     {
+    echo 'test';
 		$datafile = fopen("hd2013.csv","r");
-
-		$headers = fgetcsv($datafile);
-
-		while(!feof($datafile)){
+		$index = fgetcsv($datafile);
+		while(!feof($datafile))
+		{
 			$data = fgetcsv($datafile);
-
 			$ids[] = $data[0]; 
-			$school_array[] = array_combine($headers, $data);
-	}
+			$school_array[] = array_combine($index, $data);
+		}
 	
 	$final_array = array_combine($ids, $school_array);
-
-	fclose($file);
-
+	fclose($datafile);
 	foreach ($final_array as $key => $val) 
 	{
 		echo "<table border='1' style='width:50%'>";
 		echo "<tr>";
 		echo "<td>";
-		echo '<a href="?page='.$key.'">'.$val["INSTNM"].'</a><br>';
+		echo '<a href="index.php?page='.$key.'">'.$val["INSTNM"].'</a><br>';
 		echo "</td>";
 		echo "</tr>";
 		echo "</table>";
 	}
-	var_dump($final_array);
+	//var_dump($final_array);
+	$pagenum = $_GET['page'];
+	echo $pagenum;
 	}
-
 }
 $obj = Import::getInstance();   
-
 ?>
